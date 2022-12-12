5c2d5fca5e95a43757c03207329e5dac3cf4b070
hkuribayashi <hkuribayashi@gmail.com>
2022-12-12 15:26:36 -0300

    Alterações na classe Cliente

--- PROG2/src/exemplos/Cliente.java
+++ PROG2/src/exemplos/Cliente.java
@@ -6,6 +6,9 @@ public class Cliente {
 	public String sobrenome;
 	public String email;
 	
-	
-
+	public Cliente(String nome, String sobrenome, String email) {
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.email = email;
+	}
 }
