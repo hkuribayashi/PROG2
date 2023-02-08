fef5f519b505cad807fa3918288f67becf70d870
hkuribayashi <hkuribayashi@gmail.com>
2023-02-08 11:56:54 -0300

    Inclusão de Exception

---
+++ PROG2/src/atividade3/Data.java
@@ -0,0 +1,44 @@
+package atividade3;
+
+public class Data {
+	
+	private Integer dia;
+	private Integer mes;
+	private Integer ano;
+
+	public Data(Integer d, Integer m, Integer a) {
+		this.valData(d, m, a);
+		this.dia = d;
+		this.mes = m;
+		this.ano = a;
+	}
+	
+public void valData(Integer d, Integer m, Integer a) {
+	if (a >= 2023){
+		if (m >= 1 && m <= 12) {
+			if (m == 4 || m == 6 || m == 9 || m == 11) {
+			  if (!(d >= 1 && d <= 30))// Meses de 30 dias
+			   throw new RuntimeException("Mes "+m+
+					                      " deve ter 30"
+					                      + "dias");
+			}else if (m == 2) { // Fevereiro
+				if (!(d >= 1 && (d <=28 || d<=29)))
+				 throw new RuntimeException("Fevereiro "
+				 	+ "deve ter entre 1 e 28/29 dias");
+			}else { // Meses de 31 dias
+				if (!(d >= 1 && d <= 31))
+				 throw new RuntimeException("Mes "+m+
+		                      " deve ter 31 dias");
+			}
+		} else throw new RuntimeException("Mes Inválido");
+
+	}else throw new RuntimeException("Ano inválido");
+}
+
+
+
+
+
+
+
+}
---
+++ PROG2/src/atividade3/TestaData.java
@@ -0,0 +1,13 @@
+package atividade3;
+
+public class TestaData {
+
+	public static void main(String[] args) {
+		
+		Data d1 = new Data(31, 04, 2023);
+		
+		System.out.println(d1);
+		
+	}
+
+}
