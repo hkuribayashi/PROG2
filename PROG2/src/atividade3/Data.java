06c41c70f2c92b5a7e5f7aa655206216d6f93ee1
hkuribayashi <hkuribayashi@gmail.com>
2023-02-11 11:16:02 -0300

    Inclusão da Tarefa 3

--- PROG2/src/atividade3/Data.java
+++ PROG2/src/atividade3/Data.java
@@ -1,7 +1,7 @@
 package atividade3;
 
 public class Data {
-	
+
 	private Integer dia;
 	private Integer mes;
 	private Integer ano;
@@ -13,27 +13,51 @@ public class Data {
 		this.ano = a;
 	}
 	
-public void valData(Integer d, Integer m, Integer a) {
-	if (a >= 2023){
-		if (m >= 1 && m <= 12) {
-			if (m == 4 || m == 6 || m == 9 || m == 11) {
-			  if (!(d >= 1 && d <= 30))// Meses de 30 dias
-			   throw new RuntimeException("Mes "+m+
-					                      " deve ter 30"
-					                      + "dias");
-			}else if (m == 2) { // Fevereiro
-				if (!(d >= 1 && (d <=28 || d<=29)))
-				 throw new RuntimeException("Fevereiro "
-				 	+ "deve ter entre 1 e 28/29 dias");
-			}else { // Meses de 31 dias
-				if (!(d >= 1 && d <= 31))
-				 throw new RuntimeException("Mes "+m+
-		                      " deve ter 31 dias");
-			}
-		} else throw new RuntimeException("Mes Inválido");
+	public Integer getDia() {
+		return this.dia;
+	}
+	
+	public Integer getMes() {
+		return this.mes;
+	}
+	
+	public Integer getAno() {
+		return this.ano;
+	}
+	
+	public void setDia(Integer d) {
+		this.dia = d;
+	}
+	
+	public void setMes(Integer m) {
+		this.mes = m;
+	}
+	
+	public void setAno(Integer a) {
+		this.ano = a;
+	}
 
-	}else throw new RuntimeException("Ano inválido");
-}
+	private void valData(Integer d, Integer m, Integer a) {
+		if (a >= 2023){
+			if (m >= 1 && m <= 12) {
+				if (m == 4 || m == 6 || m == 9 || m == 11) {
+					if (!(d >= 1 && d <= 30))// Meses de 30 dias
+						throw new RuntimeException("Mes "+m+
+								" deve ter 30"
+								+ "dias");
+				}else if (m == 2) { // Fevereiro
+					if (!(d >= 1 && (d <=28 || d<=29)))
+						throw new RuntimeException("Fevereiro "
+								+ "deve ter entre 1 e 28/29 dias");
+				}else { // Meses de 31 dias
+					if (!(d >= 1 && d <= 31))
+						throw new RuntimeException("Mes "+m+
+								" deve ter 31 dias");
+				}
+			} else throw new RuntimeException("Mes Inválido");
+
+		}else throw new RuntimeException("Ano inválido");
+	}
 
 
 
--- PROG2/src/atividade3/TestaData.java
+++ PROG2/src/atividade3/TestaData.java
@@ -4,9 +4,13 @@ public class TestaData {
 
 	public static void main(String[] args) {
 		
-		Data d1 = new Data(31, 04, 2023);
+		Data d1 = new Data(30, 04, 2023);
+	
+		System.out.println(d1.getDia());
 		
-		System.out.println(d1);
+		d1.setDia(41);
+		
+		System.out.println(d1.getDia());
 		
 	}
 
